052219b7989ede5c9827554231d1648163b73556
Sabina Maria B <p_sabinamaria@yahoo.com>
2021-02-24 16:57:33 +0100

    triggered the action mainToSecond

--- app/src/main/java/com/sabina/navdemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/sabina/navdemo/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import com.sabina.navdemo.R
 
 class MainFragment : Fragment() {
@@ -13,18 +15,33 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance() = MainFragment()
     }
+    private lateinit var button: Button
 
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val view= inflater.inflate(R.layout.main_fragment, container, false)
+        button = view.findViewById(R.id.button)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
+        button.setOnClickListener {
+            // get a reference to the navigation controller and calls the navigate() method on it, arg- the resource ID of the navigation action
+            Navigation.findNavController(it).navigate( R.id.mainToSecond)
+         }
+
+
+        /*
+         the Navigation class also includes a method createNavigateOnClickListener() which provides a more efficient way of setting up a listener and navigating to a destination.
+          The same result can be achieved, therefore, using the following single line of code to initiate the transition:
+         button.setOnClickListener(Navigation.createNavigateOnClickListener( R.id.mainToSecond, null))
+         */
     }
 
 }
